1b05c475677eb6a8a017ec7a898d104da4c0639e
Jose Levy <clic00@yahoo.com>
2018-02-09 23:29:55 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8082"
+
+    baseUrl: "http://localhost:8082",
+    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/clic00-spring-ionic"
 }
