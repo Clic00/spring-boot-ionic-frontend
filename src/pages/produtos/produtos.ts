d33c7805c7d9c7aa01f04f48a8efaba57c297e2f
Clic00 <clic00@yahoo.com>
2018-03-14 16:45:44 -0300

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { CategoriaDTO } from '../../models/categoria.dto';
@@ -12,26 +12,31 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class ProdutosPage {
 
-  items : ProdutoDTO[];
-  categoria : CategoriaDTO;
-  categoria_nome : string;
+  items: ProdutoDTO[];
+  categoria: CategoriaDTO;
+  categoria_nome: string;
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
-
+    let loader = this.presentLoading();
+    loader.present();
     this.produtoService.findByCategoria(this.navParams.get('categoria_id'))
-      .subscribe( 
+      .subscribe(
         response => {
+          loader.dismiss();
           this.items = response['content'];
           this.categoria_nome = this.navParams.get('categoria_nome');
           this.loadImageUrls();
         },
-        error => {}
+        error => {
+          loader.dismiss();
+        }
       );
   }
 
@@ -42,12 +47,21 @@ export class ProdutosPage {
           response => {
             item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
           },
-          error => {}
+          error => { }
         );
-    }          
+    }
   }
 
   showDetail(produto_id: string) {
-    this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
+    this.navCtrl.push('ProdutoDetailPage', { produto_id: produto_id });
   }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde...",
+    });
+    //loader.present();
+    return loader;
+  }
+
 }
