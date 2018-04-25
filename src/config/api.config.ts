43efb84f51195703211d208063bbac3183bcaa09
Clic00 <clic00@yahoo.com>
2018-04-25 19:29:23 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
 
-    baseUrl: "http://localhost:8082",
+    // baseUrl: "http://localhost:8082",
+    baseUrl: "https://clic00-spring-ionic.herokuapp.com",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/clic00-spring-ionic"
 }
